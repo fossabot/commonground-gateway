de27e7aa73e6546f16656e282331155cfa6b07c9
Ruben van der Linde <ruben@conduction.nl>
2022-09-25 22:52:46 +0200

    Frame for data and call service

---
+++ api/src/Service/CallService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Gateway as Source ;
+
+class CallService
+{
+
+
+    public function __construct(
+    ) {
+    }
+
+}
---
+++ api/src/Service/DataService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Service;
+
+
+
+class DataService
+{
+
+    public function __construct(
+    ) {
+    }
+
+    
+
+}
